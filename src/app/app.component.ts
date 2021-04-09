1bc5fb63f76c72bc23c6ab51c2da7e9f3e665227
Isaac Hughes <isaachughesbusiness@gmail.com>
2021-04-09 16:41:47 +0100

    added clickable buttons connected to functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  display:string = '';
+  operators:string[] = ['ADD', 'SUBTRACT', 'DIVIDE', 'MULTIPLY', 'UNDO', 'C', 'DECIMAL']
+  currentSelection:string = '';
+
+  events:string[] =[];
+
+  pushToEvents = (item:string) => {
+    this.events.push(item)
+  }
+
+
+  number = (num: string) => {
+    console.log(num)
+    // if (this.currentSelection == ''){
+    //   this.currentSelection = num;
+    //   console.log(this.currentSelection)
+    // } else {
+    //   this.currentSelection + num;
+    //   console.log(this.currentSelection)
+    // }
+
+
+  }
+
+  operator = (op: string) => {
+
+    this.currentSelection = ''
+
+    if (this.operators.includes(this.events[this.events.length - 1])){
+      console.log(true)
+      this.display = 'error'
+    }else{
+      console.log(false)
+      this.events.push(op)
+    }
+
+    console.log(op)
+
+    if (op == "EQUAL"){
+      console.log(this.events)
+    }
+  }
+
 }
